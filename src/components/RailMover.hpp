c84c69e78effc021fe817398fbd89163e331da4d
ABigDumbBear <hudgens.zack@gmail.com>
2023-05-24 20:26:57 -0500

    started adding rail movement

--- src/components/RailMover.hpp
+++ src/components/RailMover.hpp
@@ -1,12 +1,15 @@
 #ifndef RAILMOVER_HPP
 #define RAILMOVER_HPP
 
+#include <set>
+
 #include "ECS.hpp"
 
 namespace StarBear {
 
 struct RailMover
 {
+  std::set<Entity> mChildren;
 };
 
 } // namespace StarBear
---
+++ src/systems/RailMoverSystem.hpp
@@ -0,0 +1,16 @@
+#ifndef RAILMOVERSYSTEM_HPP
+#define RAILMOVERSYSTEM_HPP
+
+#include "ECS.hpp"
+
+namespace StarBear {
+
+class RailMoverSystem : public System
+{
+  public:
+    void Update(Scene& aScene);
+};
+
+} // namespace StarBear
+
+#endif
